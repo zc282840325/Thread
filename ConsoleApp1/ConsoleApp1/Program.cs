3e0114eec6b3c24948e10db53da1dee967d3b908
zc282840325 <282840325@qq.com>
2019-10-18 22:36:56 +0800

    第一次提交

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static AutoResetEvent auto = new AutoResetEvent(false);
+        public static Mutex m;//线程互斥
+        private static int Count = 9;
+        static void Main(string[] args)
+        {
+            m = new Mutex();
+            Thread t1 = new Thread(setValue);
+            Thread t2 = new Thread(getValue);
+            t1.Start();
+            t2.Start();
+        }
+        public static void setValue()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                m.WaitOne();//阻塞线程
+                Console.WriteLine("a:"+ Thread.CurrentThread.ManagedThreadId);
+                m.ReleaseMutex();//释放Mutex
+            }
+        }
+        public static void getValue()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                m.WaitOne();
+                Console.WriteLine("b:" + Thread.CurrentThread.ManagedThreadId);
+                m.ReleaseMutex();
+            }
+        }
+        public static void getValue1()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                m.WaitOne();
+                Console.WriteLine("c" + Thread.CurrentThread.ManagedThreadId);
+                m.ReleaseMutex();
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/myTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class myTests
+    {
+        char last = 'c';
+        object obj = new object();
+        public void RunTest()
+        {
+            Thread th1 = new Thread(ShowNum);
+            Thread th2 = new Thread(ShowNum);
+            Thread th3 = new Thread(ShowNum);
+
+            th1.Start('a');
+            th2.Start('b');
+            th3.Start('c');
+
+
+
+        }
+
+        private void ShowNum(object name)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                lock (obj)
+                {
+                   // if ((Char)name == last + 1 || (Char)name == last - 2)//如果是连续的下一个字
+                    //{
+                        Console.WriteLine(name);
+                        last = (Char)name;
+                        Monitor.PulseAll(obj);
+                  //  }
+                 //   else
+                   // {
+                        i--;
+                        Monitor.Wait(obj);
+                  //  }
+                }
+            }
+
+        }
+    }
+}
